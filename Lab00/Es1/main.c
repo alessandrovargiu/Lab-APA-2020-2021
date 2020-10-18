2ae64136c4e41af73f53fb24232340711590bc52
alessandrovargiu <72977749+alessandrovargiu@users.noreply.github.com>
2020-10-18 12:22:07 +0200

    Update main.c

--- Lab00/Es1/main.c
+++ Lab00/Es1/main.c
@@ -9,7 +9,7 @@ int main() {
     for(i=0; i<N; i++) scanf("%d", &v[i]) ;
 
     i=0;
-    while (i<=N) {
+    while (i<=N) {          //trovo la dimensione del sottovettore di lunghezza massima
         if(v[i] == 0) {
             if(j>max) max=j;
             j=-1;
@@ -17,7 +17,7 @@ int main() {
         i++;
         j++;
     }
-    if(j>max) max=j;
+    if(j>max) max=j;        //nel caso in cui l'ultimo elemento sia non nullo il ciclo non aggiorna il max
 
     i=0;
     j=0;
@@ -27,8 +27,8 @@ int main() {
             j=0;
             continue;
         }
-        v2[j]=v[i];
-        if(j==max-1) {
+        v2[j]=v[i];         //salvo il possibile sottovettore max
+        if(j==max-1) {          //se j raggiunge il max ho trovato il sottovettore da stampare
             for(int k=0; k<max; k++) {
                 printf("%d ",v2[k]);
             }
