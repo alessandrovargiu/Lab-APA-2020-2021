c3ac0464fac4bb6fc4e9142768d402eaf5d4f6aa
alessandrovargiu <72977749+alessandrovargiu@users.noreply.github.com>
2020-10-18 12:33:08 +0200

    Update main.c

--- Lab01/Es2/main.c
+++ Lab01/Es2/main.c
@@ -12,23 +12,24 @@ int main() {
     char str[max_cod], riga[200];
     codifica c[max_cod];
     size_t j=0, k=0, m=0;
-    fp2 = fopen("C:\\Users\\admin\\Desktop\\Robba\\Materiale Polito\\II Anno\\Algoritmi e Programmazione\\2020-2021\\Lab01\\2\\dizionario.txt", "r");
+    
+    fp2 = fopen("dizionario.txt", "r");
     fscanf(fp2, "%d", &n_cod);
     for(int i=0; i<n_cod; i++){
-        fscanf(fp2, "%s %s", c[i].codice, c[i].word);
+        fscanf(fp2, "%s %s", c[i].codice, c[i].word);               //scan delle parole del dizionario, le salvo in un vettore di struct
     }
     fclose(fp2);
     fp1 = fopen("sorgente.txt", "r");
     fp3 = fopen("ricodificato.txt", "w");
 
-    while(fgets(riga, 201, fp1) != NULL) {
+    while(fgets(riga, 201, fp1) != NULL) {      //procedo riga per riga
         j=0;
-        while(j<strlen(riga)) {
+        while(j<strlen(riga)) {                 //e carattere per carattere nei cicli interni
             flag = 0;
             for (int i = 0; i < n_cod && flag == 0; i++) {
-                if(riga[j] == c[i].word[0]){        // possibile sostituzione
+                if(riga[j] == c[i].word[0]){        // possibile sostituzione (comparo con la prima lettera di una parola del dizionario)
                     k=1;
-                    while(k<strlen(c[i].word)) {
+                    while(k<strlen(c[i].word)) {            //esamino le k lettere successive 
                         if(riga[j + k] == c[i].word[k]) k++;
                         else break;
                     }
